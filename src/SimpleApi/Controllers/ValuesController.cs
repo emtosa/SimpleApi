d19395b968ddd1309fae0c79c461578b20bdb63c
Tosa Ojiru <tosao@microsoft.com>
2022-06-12 23:06:15 -0700

    Change return values

--- src/SimpleApi/Controllers/ValuesController.cs
+++ src/SimpleApi/Controllers/ValuesController.cs
@@ -9,7 +9,7 @@ public class ValuesController : ControllerBase
     [HttpGet]
     public ActionResult<IEnumerable<string>> Get()
     {
-        return new string[]{ "value1", "value2" };
+        return new string[]{ "dotnet", "playbook" };
     }
 
     // Get api/values/1
